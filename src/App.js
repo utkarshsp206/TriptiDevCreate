40a8d27ed6c050bb0958e8da693124ba531a588f
Jattzz <jattzjangra@gmail.com>
2024-03-31 00:33:37 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import HostelDashboard from './pages/HostelDashboard';
 import SurplusForm from './pages/SurplusForm';
-
+import Launching from './pages/Launching'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 function App() {
   return (
    <>
-   <Navbar/>
-   <Footer/>
+   <HostelDashboard/>
    </>
   );
 }
-
 export default App;
